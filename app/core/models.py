bc1fd1a09ecbdc2aa577d1994de323636a17951a
unnome <unnome@gmail.com>
2022-05-21 01:31:07 +0200

    fixed linting troubles

--- app/core/models.py
+++ app/core/models.py
@@ -2,7 +2,11 @@
 Database models.
 """
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+)
 
 
 class UserManager(BaseUserManager):
@@ -28,7 +32,6 @@ class UserManager(BaseUserManager):
         return user
 
 
-
 class User(AbstractBaseUser, PermissionsMixin):
     """User in the system."""
     email = models.EmailField(max_length=255, unique=True)
@@ -36,9 +39,6 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
-    objects =UserManager()
+    objects = UserManager()
 
     USERNAME_FIELD = "email"
-
-
-
